3b57c6ded452097585533ea7da05645896f5b747
joeljulianace <joeljulianace@gmail.com>
2016-08-15 14:59:53 +0530

    first maven commit

---
+++ src/test/java/com/learntestng/AppTest.java
@@ -0,0 +1,17 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+//run java file from command prompt - maven
+//run testng.xml from maven cmd line
+// generate surefire reports
+//xslt reports
+public class AppTest {
+	
+	@Test
+	public void sendMail(){
+		System.out.println("*************App Test***************");
+		
+	}
+
+}
